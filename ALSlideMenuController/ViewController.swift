9e9dbdf4fb28330e0cfa6c55d82b7f16662bb211
ArpitLokwani <arpit.lokwani@gmail.com>
2017-03-25 21:13:27 +0530

    Initial Commit

---
+++ ALSlideMenuController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALSlideMenuController
+//
+//  Created by Arpit Lokwani on 3/25/17.
+//  Copyright © 2017 Arpit Lokwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
