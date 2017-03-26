17e6b68290c226008ac52dfad6bd385797ab07c3
ArpitLokwani <arpit.lokwani@gmail.com>
2017-03-26 15:50:09 +0530

    Initial Commit

--- ALSlideMenuController/ViewController.swift
+++ ALSlideMenuController/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    var menuShowing = false
+    
+    @IBOutlet weak var menuView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openMenu(_ sender: AnyObject) {
+        if (menuShowing) {
+            self.hideMenu()
+        }else{
+           self.showMenu()
+        }
+        
+        UIView.animate(withDuration: 0.3) { 
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell!
+
+        cell.textLabel?.text = "Cell \(indexPath.row)"
+        cell.accessoryType  = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print("You tapped cell number \(indexPath.row).")
+        self.hideMenu()
+
+        if(indexPath.row == 0){
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+        }
+        else{
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+            self.navigationController?.pushViewController(nextViewController, animated: true)
+        }
+        
+        
+    }
+    
+    
+    func hideMenu() -> Void {
+        leadingConstraint.constant = -200
+        menuView.layer.shadowOpacity = 0
+        menuShowing = !menuShowing
+        
+    }
+    func showMenu() -> Void {
+        
+        leadingConstraint.constant = 0
+        menuView.layer.shadowOpacity = 1
+        menuShowing = !menuShowing
+
+    }
 
 }
 
